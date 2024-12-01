941135d5605c07222de9bda7b5ff0d3f7fafdf3d
Khawlaowl <Khawlahilli20045@gmail.com>
2024-12-01 23:12:55 +0000

    Ajout des scripts de données de test

---
+++ database-script-0x02/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO User (user_id, first_name, last_name, email, password_hash, role)
+VALUES
+    ('uuid1', 'John', 'Doe', 'john.doe@example.com', 'hashed_password', 'guest'),
+    ('uuid2', 'Jane', 'Smith', 'jane.smith@example.com', 'hashed_password', 'host');
+-- Ajoutez des données pour les autres tables ici
+
