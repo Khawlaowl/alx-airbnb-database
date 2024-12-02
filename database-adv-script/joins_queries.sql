a5ed1b368d562d8d714d7b965fbefe86c7e611d9
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2024-12-02 01:40:08 +0100

    f

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -8,9 +8,11 @@ SELECT
 FROM 
     Booking b
 INNER JOIN 
-    User u ON b.user_id = u.user_id;
+    User u ON b.user_id = u.user_id
+ORDER BY 
+    b.booking_id;
 
--- Query 2: LEFT JOIN to retrieve all properties and their reviews
+-- Query 2: LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews
 SELECT 
     p.property_id, 
     p.name AS property_name, 
@@ -19,9 +21,11 @@ SELECT
 FROM 
     Property p
 LEFT JOIN 
-    Review r ON p.property_id = r.property_id;
+    Review r ON p.property_id = r.property_id
+ORDER BY 
+    p.property_id;
 
--- Query 3: FULL OUTER JOIN to retrieve all users and bookings
+-- Query 3: FULL OUTER JOIN to retrieve all users and bookings, even if the user has no booking or a booking is not linked to a user
 SELECT 
     u.user_id, 
     u.first_name, 
@@ -31,4 +35,6 @@ SELECT
 FROM 
     User u
 FULL OUTER JOIN 
-    Booking b ON u.user_id = b.user_id;
+    Booking b ON u.user_id = b.user_id
+ORDER BY 
+    u.user_id;
