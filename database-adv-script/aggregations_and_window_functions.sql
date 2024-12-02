107bf2336e803c20b2b970e5ec8ee99292c260bb
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2024-12-02 11:21:34 +0100

    f

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -19,3 +19,16 @@ LEFT JOIN
     Booking b ON p.property_id = b.property_id
 GROUP BY 
     p.property_id, p.name;
+
+-- Query 3: Assign a unique number to each property based on total bookings using ROW_NUMBER()
+SELECT 
+    p.property_id, 
+    p.name AS property_name, 
+    COUNT(b.booking_id) AS total_bookings, 
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_number 
+FROM 
+    Property p
+LEFT JOIN 
+    Booking b ON p.property_id = b.property_id
+GROUP BY 
+    p.property_id, p.name;
